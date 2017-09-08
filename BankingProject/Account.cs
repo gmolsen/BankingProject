ebfb87bab0fd990bb50237dc959cf6053ba6537f
Greg <gmolsenvideo@gmail.com>
2017-09-08 09:48:41 -0400

    classes!

---
+++ BankingProject/Account.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking {
+	class Account {
+		//Properties goe shere
+		private double Balance = 0;
+		private string Name = "Checking";
+		private int Number = 0;
+		// methods go here
+		private bool IsAmountInvalid (double amount) {
+			if (amount > 0) {
+				return false ;
+			} else {
+				return true;
+			}
+		}
+
+		public void Deposit(double amount) {
+			if (IsAmountInvalid(amount) == true) {
+				Console.WriteLine("The amount must be greater than zero.");
+				return;
+			}
+			Balance += amount;
+		}
+		public void Withdraw (double amount) {
+			if (IsAmountInvalid(amount) == true) {
+				Console.WriteLine("The amount must be greater than zero.");
+				return;
+			}
+		}
+		public double CheckBalance() {
+				return Balance;
+		}
+		public void SetName(string name) {
+			Name = name;
+		}
+	}
+}
--- BankingProject/Program.cs
+++ BankingProject/Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BankingProject {
+namespace Banking {
 	class Program {
+
+		void Run() {
+			Account checking = new Account();
+			checking.SetName("Checking 1");
+			checking.Withdraw(100.00);
+			checking.Deposit(50.00);
+			checking.Deposit(-100.00);
+			Console.WriteLine($"Account {checking.GetName()}, balance is {checking.CheckBalance()}");
+		}
+
 		static void Main(string[] args) {
+			new Program().Run();
 		}
 	}
 }
